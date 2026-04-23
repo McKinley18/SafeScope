76968ff2bbcf4db74ce1e2ad3ab0ae4c61d2e8df
Chris McKinley <mckinley.christopherd@gmail.com>
2026-04-22 20:57:07 -0400

    Enable CORS for frontend access

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
   
