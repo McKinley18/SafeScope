c49cd1b43a67d3c1c7b7e9a859d8b58463793c6f
Chris McKinley <mckinley.christopherd@gmail.com>
2026-05-06 01:08:13 -0400

    Replace react-native useColorScheme hook with web version

--- frontend/hooks/use-color-scheme.ts
+++ frontend/hooks/use-color-scheme.ts
@@ -1,1 +1,22 @@
-export { useColorScheme } from 'react-native';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export function useColorScheme(): 'light' | 'dark' {
+  const [scheme, setScheme] = useState<'light' | 'dark'>('light');
+
+  useEffect(() => {
+    const media = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const updateScheme = () => {
+      setScheme(media.matches ? 'dark' : 'light');
+    };
+
+    updateScheme();
+
+    media.addEventListener('change', updateScheme);
+    return () => media.removeEventListener('change', updateScheme);
+  }, []);
+
+  return scheme;
+}
